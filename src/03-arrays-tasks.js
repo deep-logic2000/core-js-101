d4c100cc15d59cd3d477453c42d2ddfef56136b4
Kirill Nazarov <deeplogic2000@gmail.com>
2023-06-05 21:20:46 +0300

    feat: add all solutions of 3 task

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -562,8 +562,20 @@ function distinct(arr) {
  *    "Poland" => ["Lodz"]
  *   }
  */
-function group(/* array, keySelector, valueSelector */) {
-  throw new Error('Not implemented');
+function group(array, keySelector, valueSelector) {
+  const initialValue = new Map();
+  return array.reduce((acc, curr) => {
+    const accHasKey = acc.has(keySelector(curr));
+    let currValues;
+    if (!accHasKey) {
+      currValues = [valueSelector(curr)];
+    } else {
+      const temp = acc.get(keySelector(curr));
+      temp.push(valueSelector(curr));
+      currValues = temp;
+    }
+    return acc.set(keySelector(curr), currValues);
+  }, initialValue);
 }
 
 /**
@@ -579,8 +591,8 @@ function group(/* array, keySelector, valueSelector */) {
  *   [[1, 2], [3, 4], [5, 6]], (x) => x     =>   [ 1, 2, 3, 4, 5, 6 ]
  *   ['one','two','three'], (x) => x.split('')  =>   ['o','n','e','t','w','o','t','h','r','e','e']
  */
-function selectMany(/* arr, childrenSelector */) {
-  throw new Error('Not implemented');
+function selectMany(arr, childrenSelector) {
+  return arr.flatMap(childrenSelector);
 }
 
 /**
@@ -595,8 +607,8 @@ function selectMany(/* arr, childrenSelector */) {
  *   ['one','two','three'], [2]       => 'three'  (arr[2])
  *   [[[ 1, 2, 3]]], [ 0, 0, 1 ]      => 2        (arr[0][0][1])
  */
-function getElementByIndexes(/* arr, indexes */) {
-  throw new Error('Not implemented');
+function getElementByIndexes(arr, indexes) {
+  return indexes.reduce((acc, curr) => acc[curr], arr);
 }
 
 /**
@@ -617,8 +629,12 @@ function getElementByIndexes(/* arr, indexes */) {
  *   [ 1, 2, 3, 4, 5, 6, 7, 8 ]   =>  [ 5, 6, 7, 8, 1, 2, 3, 4 ]
  *
  */
-function swapHeadAndTail(/* arr */) {
-  throw new Error('Not implemented');
+function swapHeadAndTail(arr) {
+  const isOddLength = arr.length % 2 !== 0;
+  const middle = isOddLength ? Math.ceil(arr.length / 2) : null;
+  const head = arr.slice(0, middle ? middle - 1 : arr.length / 2);
+  const tail = arr.slice(middle || arr.length / 2, arr.length);
+  return middle ? [...tail, middle && middle, ...head] : [...tail, ...head];
 }
 
 module.exports = {
